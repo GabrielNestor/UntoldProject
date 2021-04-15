28e302c0b5b9f8e2bfa8b45c526429f2b118923c
Gabriel Lucian Nestor <lucian.nestor@gmail.com>
2021-04-15 19:08:54 +0300

    Update Untold.java

--- Untold.java
+++ Untold.java
@@ -4,6 +4,9 @@ public abstract class Untold {
     int varsta;
     boolean vaccinat;
     String CNP;
-    
+    // Sunt prezente atributele care vor fi mostenite de toate clasele.
     public abstract boolean accesFestival();
+    /**Metoda va fi mostenita e catre toate clasele, acestea avand obligatia
+    * de a o suprascrie functie de tipul de persoana care patrunde in festival.
+    */
 }
