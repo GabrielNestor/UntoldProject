751bd2cee1b71ea35797c1e98c1bfaa2896299b7
Gabriel Lucian Nestor <lucian.nestor@gmail.com>
2021-04-16 15:42:47 +0300

    Update intrareFestival.java

--- intrareFestival.java
+++ intrareFestival.java
@@ -13,6 +13,8 @@ public class intrareFestival extends Organizare {
         if(p.accesFestival() == true && p.bilet == true) {
             System.out.println("Distractie placuta");
         }else {System.out.println("Acces nepermis");
+               //metoda de verificare bilet se foloseste de accesFestival, din clasa Public, pentru a verifica daca persoana are peste 18 ani,
+               //este vaccinata, dar prezinta si biletul de acces
     }
     }
 }
