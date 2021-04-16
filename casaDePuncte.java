4a466ad0a9b2516e737fb3763ecb991620ea16fa
Gabriel Lucian Nestor <lucian.nestor@gmail.com>
2021-04-16 15:38:52 +0300

    Update casaDePuncte.java

--- casaDePuncte.java
+++ casaDePuncte.java
@@ -12,5 +12,6 @@ public class casaDePuncte extends Organizare {
     
     public void incarcaBratara(Public p){
         p.puncte += 10;
+        //pentru fiecare apel al metodei, aceasta va adauga cate 10 puncte pe bratara fiecarei persoane din public
     }
 }
