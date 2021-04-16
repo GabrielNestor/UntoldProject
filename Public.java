787dfbaa48f8f03b613b1ed75d97e6ee41e319f9
Gabriel Lucian Nestor <lucian.nestor@gmail.com>
2021-04-16 15:36:52 +0300

    Update Public.java

--- Public.java
+++ Public.java
@@ -2,7 +2,7 @@ public class Public extends Untold {
     boolean bilet;
     int tipBilet;
     int puncte;
-    
+    //clasa public prezzinta atribute legate de bilet, dar si un numar de puncte pe baza carora se pot face achizitii in festival
     public Public(String nume, String prenume, String CNP, int varsta, boolean vaccinat, boolean bilet, int tipBilet){
         this.nume = nume;
         this.prenume = prenume;
@@ -14,13 +14,18 @@ public class Public extends Untold {
     }
     
     public boolean accesFestival(){
-        if (varsta>18 && vaccinat==true){return true;}
+        if (varsta > 18 && vaccinat == true && bilet == true){
+            Systemm.out.println("Bine ai venit, distractie placuta");
+            return true;}
+        System.out.println("Acces nepermis");
         return false;
+        //Accesul publicului se permite atata timp cat varsta este peeste 18 ani, iar persoana prezinta biletul, impreuna cu adeverinta de vaccinare
     }
     
     public boolean accesZonaVIP() {
         if(tipBilet == 2){return true;}
         return false;
+        //in zona vip este permisa numai atunci cand tipul biletului are valoarea 2
     }
     
 }
