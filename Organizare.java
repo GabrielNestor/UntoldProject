9c8d1a50df69901bf6c90811534e5b2dd0567afd
Gabriel Lucian Nestor <lucian.nestor@gmail.com>
2021-04-16 15:29:44 +0300

    Update Organizare.java

--- Organizare.java
+++ Organizare.java
@@ -1,5 +1,6 @@
 public abstract class Organizare extends Untold {
     boolean legitimatie;
+    //legitimatia este o particularizare a clasei
     
     public boolean accesFestival() {
         if(legitimatie==true && varsta>18) {
@@ -7,6 +8,8 @@ public abstract class Organizare extends Untold {
             return true;
         }else {System.out.println("Nu stiu cine te-a chemat, dar nu ai voie");}
         return false;
+        //accesul in festival pentru persoanele din organizatie sa face atat pe baza varstei,
+        //cat si a legitimatiei
     }
     
 }
