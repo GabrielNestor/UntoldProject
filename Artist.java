16effb0316f22532093a38bf08cbeb8cc2a140fc
Gabriel Lucian Nestor <lucian.nestor@gmail.com>
2021-04-19 17:27:07 +0300

    Update Artist.java

--- Artist.java
+++ Artist.java
@@ -1,35 +1,26 @@
 public class Artist extends Untold{
     int faima;
+    String gen;
     
-    public Artist(String nume, String prenume, String CNP, int varsta, boolean vaccinat, int faima){
+    public Artist(String nume, String prenume, String CNP, int varsta, boolean vaccin,String gen, int faima){
         this.nume = nume;
         this.prenume = prenume;
         this.CNP = CNP;
         this.varsta = varsta;
-        this.vaccinat = vaccinat;
+        this.vaccin = vaccin;
         this.faima = faima;
-        //Constructorului default i se ofera parametrii, cel din clasa artistilor
-        //avand un parametru aditional(faima), care va fi folosit in metoda de repartizare pe scene.
+        this.gen = gen;
     }
     
+    public boolean equals(Object o) {
+        if(o == null) {return false;}
         
-    public boolean accesFestival() {
-        if(vaccinat==true){
-            System.out.println("Bun venit, sa ne distrati");
+        if(!(o instanceof Artist)) {return false;}
+        
+        Artist a = (Artist)o;
+        if(this.gen == a.gen && this.faima == a.faima){
             return true;}
-        else {System.out.print("Acces nepermis");}
         return false;
-        //Conditia de acces a artistilor este ca acestia sa fie vaccinati.
     }
-    
-    public String repartizareScena(){
-        if(faima>500 && faima<1000) {return "Scena mare";}
-        else if(faima<500) {return "Scena mica";}
-        return "Stadion";
-        // In functie de nivelul faimei, artistul va presta pe:
-        //  -scena mica, daca acesta are sub 500 faima;
-        //  -scena mare, daca acesta are intre 500-1000 faima;
-        //  -stadion, daca acesta are peste 1000 faima;
-         
-    }
+    //doi artisti sunt considerati egali atunci cand nivelul faimei si genul lor sunt identice.
 }
