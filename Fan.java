52b33451a1f7ef8e0606fa4bb11ae9f919439457
Gabriel Lucian Nestor <lucian.nestor@gmail.com>
2021-04-23 11:50:49 +0300

    Update Fan.java

--- Fan.java
+++ Fan.java
@@ -9,5 +9,7 @@ public abstract class Fan extends Untold{
     public boolean bratara;
     //atribute mostenite de catre ambele categorii de fani
     
-
+    public String toString(){
+        return "Nume complet: " + this.numeComplet + ", varsta: " + this.varsta + ", numar de puncte pe bratara:" + this.puncte;
+    }
 }
