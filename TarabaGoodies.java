a0495cda93650ffcdb7635751c2e6e812c06a16e
Gabriel Lucian Nestor <lucian.nestor@gmail.com>
2021-04-28 20:08:41 +0300

    Update TarabaGoodies.java

--- TarabaGoodies.java
+++ TarabaGoodies.java
@@ -1,24 +1,32 @@
 import java.util.*;
 public class TarabaGoodies extends Organizare{
+    public Map<String,Integer> stoc = new HashMap<>();
     
-    private Map<String,Integer> stoc = new HashMap<>();
     public void refacereStoc(){
+        System.out.println("Se reface stocul");
         stoc.put("Tricou Armin",80);
         stoc.put("Tricou TimmyTrumpet",80);
         stoc.put("Cana Untold", 40);
         stoc.put("Borseta Untold", 100);
+        System.out.println("Stoc Refacut");
     }
-
-
+    
+    public void afisareProduse(){
+        stoc.forEach((k,v)->System.out.println(k+ " " + v));
+        //metoda pentru afisarea tuturor produselor ramase in stoc
+    }
+    
     public TarabaGoodies(String numeComplet, int varsta){
         this.numeComplet = numeComplet;
         this.varsta = varsta;
     }
     
     public void vinde(Fan f, String goodie){
-        f.puncte -=stoc.get(goodie);
-        stoc.remove(goodie);
-    //Metoda este folosita pe personalul care lucreaza la taraba pentru a vinde fanilor produse. 
-    //Aceasta scade numarul de puncte de pe bratara fanului in functie de valoarea specificata in HashMap
+        if(stoc.containsKey(goodie)){
+            f.puncte-=stoc.get(goodie);
+            stoc.remove(goodie);
+        }else{System.out.println("Produsul ales nu este in stoc");}
+        //Metoda este folosita pe personalul care lucreaza la taraba pentru a vinde fanilor produse. 
+        //Aceasta scade numarul de puncte de pe bratara fanului in functie de valoarea specificata in HashMap.
     }
-}
+
