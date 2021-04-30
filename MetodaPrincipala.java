89fd847c4579cbbd5b74d90e22b909f043b52fdb
Gabriel Lucian Nestor <lucian.nestor@gmail.com>
2021-04-30 21:22:41 +0300

    Update MetodaPrincipala.java

--- MetodaPrincipala.java
+++ MetodaPrincipala.java
@@ -1,5 +1,19 @@
 public class MetodaPrincipala {
-    public static void main(String ... args) {
-        
+    public static void main(String[] args){
+        try{Vaccinat Vali = new Vaccinat("Constantinescu Valentin", "19721039231", 23, true, true, 0, true); 
+            Intrare Mihai = new Intrare("Mihai Andrei", 20);
+            casaPuncte Alexa = new casaPuncte("Alexa Radu", 25);
+            System.out.println(Mihai.permiteAccesFestival(Vali));
+            TarabaGoodies Andrei = new TarabaGoodies("Andrei George" , 21);
+            Alexa.incarcaBratara(Vali);
+            System.out.println(Vali.puncte);
+            Andrei.refacereStoc();
+            Andrei.afisareProduse();
+            Andrei.vinde(Vali, "Tricou Armin");
+            Andrei.afisareProduse();
+            Andrei.vinde(Vali, "Tricou Armin");
+        }catch(persoanaInfectata p){
+            System.out.println(p.getMessage());
+        }
     }
 }
