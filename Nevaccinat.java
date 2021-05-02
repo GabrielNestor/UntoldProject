125345196a92345828153dc7e2d114adb53588f8
Gabriel Lucian Nestor <lucian.nestor@gmail.com>
2021-05-02 20:19:48 +0300

    Update Nevaccinat.java

--- Nevaccinat.java
+++ Nevaccinat.java
@@ -1,18 +1,15 @@
 import java.util.Random;
 public class Nevaccinat extends Fan{
     public final boolean vaccin = false;
-    public Random virus = new Random(); 
+    public Random virus = new Random();
     public boolean covid = virus.nextBoolean();
-    //Se va importa clasa Random in scopul utilizarii metodei nextBoolean()
-    //pentru a genera o valoare aleatoare de tip boolean in cazul in care fanul este nevaccinat.
     
     public Nevaccinat(String numeComplet, String CNP, int varsta,
-                    boolean bilet, int tipBilet, boolean parinte){
+                    boolean bilet, boolean parinte){
         this.numeComplet = numeComplet;
         this.CNP = CNP;
         this.varsta = varsta;
         this.bilet = bilet;
-        this.tipBilet = tipBilet;
         this.parinte = parinte;
     }
 }
