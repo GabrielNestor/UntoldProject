4e7d1d454457fc25c14a4690ea511500c1783bee
Gabriel Lucian Nestor <lucian.nestor@gmail.com>
2021-05-02 20:25:05 +0300

    Update MetodaPrincipala.java

--- MetodaPrincipala.java
+++ MetodaPrincipala.java
@@ -1,17 +1,17 @@
-public class MetodaPrincipala {
+public class testPublic {
     public static void main(String[] args){
-        try{Vaccinat Vali = new Vaccinat("Constantinescu Valentin", "19721039231", 23, true, true, 0, true); 
+        try{Vaccinat Vali = new Vaccinat("Constantinescu Valentin", "19721039231", 23, true, true);
+            Vaccinat AAA = new Vaccinat("AAA", "19721039231", 60, true, true);
+            Vaccinat aAA = new Vaccinat("BAA", "19721039231", 23, true, true);
             Intrare Mihai = new Intrare("Mihai Andrei", 20);
-            casaPuncte Alexa = new casaPuncte("Alexa Radu", 25);
-            System.out.println(Mihai.permiteAccesFestival(Vali));
-            TarabaGoodies Andrei = new TarabaGoodies("Andrei George" , 21);
-            Alexa.incarcaBratara(Vali);
-            System.out.println(Vali.puncte);
-            Andrei.refacereStoc();
-            Andrei.afisareProduse();
-            Andrei.vinde(Vali, "Tricou Armin");
-            Andrei.afisareProduse();
-            Andrei.vinde(Vali, "Tricou Armin");
+            Nevaccinat Ana = new Nevaccinat("Ana Mihai", "123123", 30, true, true);
+            Vaccinat Vlad = new Vaccinat("Ghica Vlad", "19721034254", 24, true, true);
+            Mihai.permiteAcces(AAA);
+            Mihai.permiteAcces(aAA);
+            Mihai.permiteAcces(Ana);
+            Mihai.permiteAcces(Vali);
+            Mihai.permiteAcces(Vlad);
+            Mihai.afisareLista();
         }catch(persoanaInfectata p){
             System.out.println(p.getMessage());
         }
