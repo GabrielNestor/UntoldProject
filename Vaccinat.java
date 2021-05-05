c23eb592e73f8036e3c0d7de6123db9f3f5e4c39
Gabriel Lucian Nestor <lucian.nestor@gmail.com>
2021-05-05 20:59:06 +0300

    Update Vaccinat.java

--- Vaccinat.java
+++ Vaccinat.java
@@ -2,7 +2,6 @@ import java.util.*;
 public class Vaccinat extends Fan{
     public final boolean covid = false;
     public final boolean vaccin = true;
-    public List<String> rucsac = new ArrayList<>();
     
     public Vaccinat(String numeComplet, String CNP, int varsta, boolean bilet, boolean parinte){
         this.numeComplet = numeComplet;
@@ -10,5 +9,6 @@ public class Vaccinat extends Fan{
         this.varsta = varsta;
         this.bilet = bilet;
         this.parinte = parinte;
+        this.impachetat();
     }
 }
