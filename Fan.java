e67645df447f89010124d2426d399f6054a8ea94
Gabriel Lucian Nestor <lucian.nestor@gmail.com>
2021-05-05 20:57:34 +0300

    Update Fan.java

--- Fan.java
+++ Fan.java
@@ -6,7 +6,21 @@ public abstract class Fan extends Untold{
     public boolean covid;
     public String CNP;
     public boolean bratara;
-    //atribute mostenite de catre ambele categorii de fani
+    public List<String> rucsac = new ArrayList<>();
+    //atribute mostenite de ambele categorii de fani
+    Random r = new Random();
+    
+    List<String> obiecte = Arrays.asList("tricou", "sticla apa", "baterie externa", "ochelari",
+                                        "portofel", "buletin", "ruj", "masca", "dezinfectant", 
+                                        "crema", "casti", "bricheta", "tigari", "mancare", "telefon");
+    //o lista cu lucrurile pe care le pot avea fiecare fan
+                                        
+    protected void impachetat(){
+        for(int i=0;i<2;i++){
+            rucsac.add(obiecte.get(r.nextInt(obiecte.size())));
+    //metoda care adauga in rucsac 2 elemente la intamplare din lista
+        }
+    }
     
     public String toString(){
         return "Nume complet: " + this.numeComplet + ", varsta: " + this.varsta + ", numar de puncte pe bratara:" + this.puncte;
