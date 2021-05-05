a89183e62498d2da62a074bb40b8d8604cc5af79
Gabriel Lucian Nestor <lucian.nestor@gmail.com>
2021-05-05 21:01:41 +0300

    Update Badigard.java

--- Badigard.java
+++ Badigard.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Badigard extends Organizare{
-    
+    //se creeaza o clasa Badigard pentru perchezitionarea fanilor
     public List<String> ObiecteNepermise = Arrays.asList("sticla apa", "bricheta", "mancare");
     
     public Badigard(String numeComplet, int varsta){
@@ -13,6 +13,8 @@ public class Badigard extends Organizare{
             if(f.rucsac.contains(x)){
                 System.out.println("Nu ai voie cu " + x);
                 f.rucsac.remove(x);
+    //daca badigardul gaseste in rucsacul fanilor un element care se afla pe lista obiectelor interzise
+    //acesta il va scoate din rucsac
             }
         }
     }
